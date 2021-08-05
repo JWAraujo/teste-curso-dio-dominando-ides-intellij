b2764bb50344ce5ceb5fb4c3be41d26480d81142
JWAraujo <wt.araujo82@gmail.com>
2021-08-05 11:29:00 -0300

    criando calculadora

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,12 +3,10 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
-
     public static void main(String[] args) {
         Gato gato = new Gato();
-        System.out.println(gato);
 
-        Livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(gato);
 
         /*int a = 5;
         int b = 3;
@@ -16,36 +14,3 @@ public class PrimeiroPrograma {
     }
 }
 
-class Livro {
-    private String nome;
-    private Integer numPaginas;
-
-    public Livro(String nome, Integer numPaginas) {
-        this.nome = nome;
-        this.numPaginas = numPaginas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getNumPaginas() {
-        return numPaginas;
-    }
-
-    public void setNumPaginas(Integer numPaginas) {
-        this.numPaginas = numPaginas;
-    }
-
-    @Override
-    public String toString() {
-        return "Livro{" +
-                "nome='" + nome + '\'' +
-                ", numPaginas=" + numPaginas +
-                '}';
-    }
-}
